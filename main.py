e271ca45b75d1646658603437c645e6327bd3f22
Dasha <dashaa.r@inbox.ru>
2025-05-13 00:29:26 +0700

    add partners route

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, send_from_directory
+from flask import Flask, jsonify, send_from_directory, request
 import psycopg2
 
 app = Flask(__name__, static_folder='static')
@@ -20,6 +20,31 @@ def get_partners():
 @app.route('/api/partners')
 def partners():
     return jsonify(get_partners())
+
+@app.route('/api/addPartner', methods=['POST'])
+def add_partner():
+    data = request.get_json()
+    name = data.get('name')
+    phone = data.get('phone')
+
+    if not name or not phone:
+        return jsonify({'error': 'Имя и телефон обязательны'}), 400
+
+    try:
+        conn = psycopg2.connect(
+            dbname="exam",
+            user="postgres",
+            password="root",
+            host="localhost"
+        )
+        cur = conn.cursor()
+        cur.execute("INSERT INTO partners (name, phone) VALUES (%s, %s)", (name, phone))
+        conn.commit()
+        cur.close()
+        conn.close()
+        return jsonify({'message': 'Партнёр успешно добавлен'}), 201
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
